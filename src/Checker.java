5a013c6efb327153de20374b53904155217a5d63
Yarik <yaroslav.boberskiy@gmail.com>
2017-11-14 23:22:05 +0200

    Account balance example with getters and setters.

--- src/Checker.java
+++ src/Checker.java
@@ -2,19 +2,7 @@ public class Checker {
 
 
     public static void main(String[] args) {
-        Clock clock = new Clock("11:21", "12:30");
-        Lamp lamp = new Lamp("blue", 12, true);
 
-        Room room = new Room(clock, lamp);
-
-        room.getClock().setCurrentTime("23:00");
-        room.getClock().setAlarmTime("23:01");
-
-        room.getLamp().pushSwitchButton();
-        room.getLamp().pushSwitchButton();
-        room.getLamp().pushSwitchButton();
-        room.getLamp().pushSwitchButton();
-        room.getLamp().pushSwitchButton();
 
     }
 }
