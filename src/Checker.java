42342c56f8ab665d77d06db3b87ece44654e2b02
Yarik <yaroslav.boberskiy@gmail.com>
2017-11-19 22:25:36 +0200

    Account balance example with getters and setters.

--- src/Checker.java
+++ src/Checker.java
@@ -2,17 +2,56 @@ import java.util.Scanner;
 
 public class Checker {
 
+    private static MobilePhone phone = new MobilePhone();
+
     public static void main(String[] args) {
-        int[] inputArray = new int[5];
-        System.out.println("Input Array Size is: " + inputArray.length);
-        int newArrayLength = 8;
-        inputArray = ResizeArray (inputArray, newArrayLength);
-        System.out.println("New Array Size is: " + inputArray.length);
+        while (true) {
+            ManageContacts();
+        }
     }
 
-    private static int[] ResizeArray (int[] inputArray, int newArrayLength) {
-        inputArray = new int[newArrayLength];
-        return inputArray;
+    public static void Menu () {
+        System.out.println("==== PHONE ADDRESS BOOK ====");
+        System.out.println("Enter 1 to add new contact");
+        System.out.println("Enter 2 to list all contacts");
+        System.out.println("Enter 3 to modify existing contact");
+        System.out.println("Enter 4 to remove existing contact");
     }
 
+    public static void ManageContacts () {
+        Menu();
+        Scanner menuInput = new Scanner(System.in);
+        Scanner nameInput = new Scanner(System.in);
+        Scanner telNumInput = new Scanner(System.in);
+
+        String name;
+        String telNum;
+
+        switch (menuInput.nextInt()) {
+            case 1:
+                System.out.println("Enter new contact name");
+                name = nameInput.nextLine();
+                System.out.println("Enter new contact tel. num");
+                telNum = telNumInput.nextLine();
+                phone.AddContact(new Contact(name, telNum));
+                break;
+
+            case 2:
+                phone.ShowContacts();
+                break;
+
+            case 3:
+                System.out.println("Enter name to modify");
+                name = nameInput.nextLine();
+                System.out.println("Enter tel to modify");
+                telNum = telNumInput.nextLine();
+                phone.UpdateContact(name, telNum);
+                break;
+
+            case 4:
+                System.out.println("Enter contacts name to remove");
+                name = nameInput.nextLine();
+                phone.RemoveContact(name);
+        }
+    }
 }
